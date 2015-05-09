01f4a93a9fa4a8a06a22edd333ee9c24a6664608
EderVs <eedervs.97@hotmail.com>
2015-05-09 17:45:45 -0500

    All gifs template

--- TinderOfGifs/urls.py
+++ TinderOfGifs/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from gif.views import AllGifsView
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^api/', include('api.urls', namespace='api')),
+    url(r'^gifs/$', AllGifsView.as_view(), name='get_all_gifs'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- gif/views.py
+++ gif/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, JsonResponse
+from django.views.generic import View
+
+from gif.models import Gif, Vote
 
 # Create your views here.
+class AllGifsView(View):
+	def get(self, request):
+		gifs = Gif.objects.all()
+		gifs_to_show = []
+		for gif in gifs:
+			votes = len(Vote.objects.filter(Gif=gif))
+			gifs_to_show.append({'url': gif.url, 'votes': votes})
+		context = {
+			'gifs': gifs_to_show,
+		}
+		return render(request, "gif/all_gifs.html", context)
